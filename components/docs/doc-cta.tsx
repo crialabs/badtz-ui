64f6b73a9acdd2fd99e0f38ecd3822ba55221529
badtzx0 <admin@badtz-ui.com>
2025-04-27 13:16:34 +0200

    fixed mouse-wave-shader - nextjs15

--- components/docs/doc-cta.tsx
+++ components/docs/doc-cta.tsx
@@ -21,8 +21,8 @@ export default function DocsCta({ className }: { className?: string }) {
           Build stunning websites even faster.
         </h3>
         <p className="!text-[13.5px] text-sidebar-muted-foreground mt-4 mb-6">
-          Get BadtzUI Pro and access over 70+ prebuilt templates, blocks, and
-          sections for React.
+          Get BadtzUI Pro and access prebuilt templates, blocks, and sections
+          for ReactJS.
         </p>
         <span className="h-8 flex items-center text-[13px] w-full justify-center whitespace-nowrap rounded-md font-medium px-3 bg-accent hover:bg-[#6370c8] border border-[#6370c8] text-white transition-colors duration-200 relative gap-2 [&_svg]:mb-[1.5px] [&_svg]:pointer-events-none [&_svg]:size-3.5 [&_svg]:shrink-0">
           BadtzUI Pro <ExternalLinkIcon aria-hidden="true" />
